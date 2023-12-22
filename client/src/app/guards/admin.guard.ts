8c62ad04411cc25d0338229ef8794840b69b0113
Irakli <iraklichabiev7@gmail.com>
2023-12-22 15:45:33 +0400

    Guards added

--- client/src/app/guards/admin.guard.ts
+++ client/src/app/guards/admin.guard.ts
@@ -27,12 +27,13 @@ export class AdminGuard implements CanActivate {
   }
   private decodeToken(token: string | null): any{
     if(!token) return null;
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayLoad = decodeURIComponent(atob(base64).split('').map(c => '%'+ ('00'+ c.charCodeAt(0).toString(16)).slice(-2)).join(''));
-    return JSON.parse(jsonPayLoad);
-   }
 
-  
+    try{
+      return JSON.parse(atob(token.split('.')[1]));
+    }catch(e){
+      console.error('Error decoding JWT token', e);
+      return null;
+    }
+   }  
    
 }
